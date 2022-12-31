66a182e66f8982ac0829024bc23101098bf0c8aa
kangactor123 <kangactor123@naver.com>
2022-12-31 11:31:03 +0900

    _app 에서 Query Client state 에 담아둠

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import { globalStyle } from 'src/common/styles/global';
@@ -11,9 +11,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
  * 초기화 로직을 컨트롤 할 수 있음
  */
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
