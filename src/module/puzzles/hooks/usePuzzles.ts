83a6c6b9edb9d2fe3f62f78a7616029cd1696f63
bleuuue <bleuuue@gmail.com>
2023-03-24 14:42:48 +0900

    puzzle-list api

---
+++ src/module/puzzles/hooks/usePuzzles.ts
@@ -0,0 +1,9 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { ApiError } from 'src/core/type/ApiError';
+import { fetchPuzzles } from '../api';
+import { PUZZLES_KEY } from '../key';
+import { Puzzle } from '../types';
+
+export const usePuzzles = (options?: UseQueryOptions<Puzzle[], ApiError>) => {
+  return useQuery<Puzzle[], ApiError>([PUZZLES_KEY], () => fetchPuzzles(), options);
+};
