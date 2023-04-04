4b8f74ede649328f6f414f650c00066ef16087df
kangactor123 <kangactor123@naver.com>
2023-04-04 21:34:36 +0900

    update

--- src/module/puzzles/hooks/usePuzzles.ts
+++ src/module/puzzles/hooks/usePuzzles.ts
@@ -5,8 +5,5 @@ import { PUZZLES_KEY } from '../key';
 import { Puzzle } from '../types';
 
 export const usePuzzles = (options?: UseQueryOptions<Puzzle[], ApiError>) => {
-  return useQuery<Puzzle[], ApiError>([PUZZLES_KEY], () => fetchPuzzles(), {
-    retry: false,
-    ...options,
-  });
+  return useQuery<Puzzle[], ApiError>([PUZZLES_KEY], () => fetchPuzzles(), options);
 };
