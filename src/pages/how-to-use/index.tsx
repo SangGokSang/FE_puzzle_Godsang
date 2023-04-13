5ddf7c02e649ac9d5f6520b8cc355fa7b0e7f7f7
kangactor123 <kangactor123@naver.com>
2023-04-13 19:08:34 +0900

    how-to-use-img

--- src/pages/how-to-use/index.tsx
+++ src/pages/how-to-use/index.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import Layout from 'src/components/common/Layout';
 import styled from '@emotion/styled';
 import Image from 'next/image';
-import symbol_img from 'public/assets/images/main-symbol.png';
 import { css } from '@emotion/css';
 
 const HowToUseWrap = styled.div`
   width: 340px;
-  height: 560px;
+  height: 100%;
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: #fff;
   border-radius: 15px;
+
+  .add-area {
+    width: 100%;
+    height: 60px;
+  }
 `;
 
 // 이용방법과 광고가 들어갑니다.
@@ -17,7 +25,8 @@ export default function HowToUse() {
   return (
     <Layout>
       <HowToUseWrap>
-        <Image src="/assets/images/how-to-use.png" alt="how-to-use" width="340" height="0" />
+        <Image src="/assets/images/how-to-use.png" alt="how-to-use" width="320" height="480" />
+        <div className="add-area">광고를 달아주세욤</div>
       </HowToUseWrap>
     </Layout>
   );
