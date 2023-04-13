8a581f6998c5c35db2bdc99b081b8c9b43e5e053
bleuuue <bleuuue@gmail.com>
2023-04-13 20:27:40 +0900

    thumbnail 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,6 @@ export default function Document() {
         <meta name="title" property="og:title" content="Dear My 2023" />
         <meta name="description" property="og:description" content="우리에게 선물로 다가온 시간을 채워봐요." />
         <meta name="image" property="og:image" content="/assets/images/thumbnail.png" />
-        {/* <meta name="url" property="og:url" content="https://dearmy2023.click" /> */}
         <meta property="og:locale" content="ko_KR" />
       </Head>
       <body>
